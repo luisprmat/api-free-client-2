592907bff5bc1524c045c8ba5d7ee628a744f349
Luis Antonio Parrado <luisprmat@gmail.com>
2021-08-31 00:51:20 -0500

    Create route for redirect to API

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -9,7 +9,10 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 bg-white border-b border-gray-200">
-                    You're logged in!
+                    Solicitar permisos
+                    <a href="{{ route('redirect') }}" class="ml-4 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+                        Redirigir
+                    </a>
                 </div>
             </div>
         </div>
