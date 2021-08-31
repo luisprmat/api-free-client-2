a409e0a67edd14500987dcfc9422dfad9bacca2d
Luis Antonio Parrado <luisprmat@gmail.com>
2021-08-31 00:30:47 -0500

    Add free api credentials as service

--- config/services.php
+++ config/services.php
@@ -30,4 +30,9 @@ return [
         'region' => env('AWS_DEFAULT_REGION', 'us-east-1'),
     ],
 
+    'api_free' => [
+        'client_id' => env('API_FREE_CLIENT_ID'),
+        'client_secret' => env('API_FREE_CLIENT_SECRET'),
+    ],
+
 ];
